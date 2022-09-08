fef838225afbd0a29ca3f9fbf06409246d8c1964
ferferfer2 <63882963+ferferfer2@users.noreply.github.com>
2022-09-08 15:59:57 -0300

    Add files via upload

---
+++ uc_10_s1_atv2/atv2.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE atv2;
+
+USE atv2;
+
+CREATE TABLE Unidades
+(
+cd_unidade INT PRIMARY KEY AUTO_INCREMENT,
+ds_unidade VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Produtos
+(
+cd_produto INT PRIMARY KEY AUTO_INCREMENT,
+ds_produto VARCHAR(200) NOT NULL,
+vl_produto DOUBLE NOT NULL,
+qt_produto DOUBLE NOT NULL,
+cd_unidade INT NOT NULL,
+CONSTRAIN fk_produto_unidade FOREIGN KEY(cd_unidade) REFERENCES Unidades (cd_unidade)
+);
